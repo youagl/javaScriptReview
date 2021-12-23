e9f71d2bde88246228f9741963c8244c83121aed
yoyolee <yoyolee_job@163.com>
2021-12-23 11:43:30 +0800

    async执行顺序分析

--- promise_async/使用async改写promise.js
+++ promise_async/使用async改写promise.js
@@ -46,3 +46,8 @@ async function doIt_byAsync(){
     console.timeEnd();
 }
 // doIt_byAsync();
+
+//async执行顺序分析
+//敲黑板的点：1）async的函数也是宏任务，只是返回的Promise的回调是微任务，回调结果会在微任务里执行。
+        //   2）遇到await时，后面的代码相当于放到了一个定时器里执行。
+        //   3) 函数如果没有return，则返回值为undefined
